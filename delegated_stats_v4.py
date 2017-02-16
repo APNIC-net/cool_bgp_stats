Fixes on delegation stats computation and try to make delegated_stats script perform better
delegated_stats_v4.py was created. Instead of initializing the stats_df
DataFrame with all the possible combinations of the index columns
values, rows are created when necessary. I thought this would perform
better, however, the fact of having to sort the index after adding a
new row probably makes it perform worse. Indeed, comparing the
execution of v3 of the script and v4 of the script I found out that v3
is faster.

delegated_stats_v4.py
@@ -1,93 +1,15 @@
 #! /usr/bin/python2.7 
 # -*- coding: utf8 -*-
 
-import sys, getopt
+import os, sys, getopt
 import numpy as np
 import pandas as pd
 import datetime
 # Just for DEBUG
-#import os
 #os.chdir('/Users/sofiasilva/GitHub/cool_bgp_stats')
 from DelegatedHandler import DelegatedHandler
 
 statistics = ["NumOfDelegations", "NumOfResources", "IPCount", "IPSpace"]
-    
-# This function initializes the DataFrame that will be used to store the computed statistics
-def initializeStatsDF(del_handler, EXTENDED, stat):
-    stats_df = pd.DataFrame()
-
-    # For each organization that appears in the del_handler object
-    for o in del_handler.orgs:
-        # For each resource type
-        for r in del_handler.res_types:
-            # Set the status variable with its possible values according to the resource type
-            if r == 'All':
-                status = ['All']
-            elif r == 'asn':
-                if o == 'All':
-                    status = del_handler.status_asn_all
-                elif o == 'NA':
-                    status = del_handler.status_notdel
-                else:
-                    status = del_handler.status_asn_countries
-            else: # r == 'ipv4' or r == 'ipv6'
-                if o == 'All':
-                    status = del_handler.status_ip_all
-                elif o == 'NA':
-                    status = del_handler.status_notdel
-                else:
-                    status = del_handler.status_ip_countries
-                
-            # The names of the columns that will be part of the index
-            index_names = ['Geographic Area',
-                           'ResourceType',
-                           'Status',
-                           'Organization',
-                           'Date'
-                           ]
-            
-            # The possible values of each column that is part of the index
-            if EXTENDED:
-                iterables = [del_handler.orgs_areas[o], [r], status, [o], list(del_handler.dates_range)]
-                                
-            else:
-                iterables = [del_handler.orgs_areas[o], [r], status, 'NA', list(del_handler.dates_range)]
-            
-            # If a specific stat is not provided, all the stats will be computed
-            if stat == '':
-                cols = [
-            
-            # NumOfDelegations counts the number of rows in the delegated file
-                                                           'NumOfDelegations',
-            # For IPv4 or IPv6 NumOfResources = NumOfDelegations as IP blocks delegations
-            # are shown in the delegated files as one block per line
-            # For ASNs NumOfResources counts the number of ASNs assigned which is not
-            # necessarily equal to NumOfDelegations as more than one ASN may be assigned
-            # as part of a single delegation
-                                                           'NumOfResources',
-            # IPCount counts the number of IPs assigned/allocated for IPv4
-            # and the number of /56s for IPv6.
-            # For ASNs IPCount is nan
-                                                           'IPCount',
-            # IPSpace contains the number of /24s assigned/allocated for IPv4
-            # and the number of /48s for IPv6.
-            # For ASNs IPSpace is nan
-                                                           'IPSpace'                        
-                        ]
-            else:
-                cols = [stat]
-                
-            # An index with all the possible combinations of the values of the index columns
-            index = pd.MultiIndex.from_product(iterables, names=index_names)
-            # An empty DataFrame with the index we created and the columns for the stats that will be computed
-            aux_df = pd.DataFrame(index=index, columns=cols)
-
-            # The new DataFrame is appended to the existing stats DataFrame the function received as a parameter
-            stats_df = pd.concat([stats_df, aux_df])
-
-    # We sort the index and return the DataFrame    
-    stats_df.sort_index(inplace=True)
-    return stats_df
 
 # This function computes 
 def compute56s(prefix_length_array):
@@ -96,7 +18,65 @@ def compute56s(prefix_length_array):
 def compute48s(prefix_length_array):
     return sum(pow(2, 48 - prefix_length_array))
     
-def computation_loop(delegated_subset, a, r, d, o, stats_df, dates_range):
+def updateOrInsertIntoStatsDF(stats_df, area, res_type, status, org, date, stat_name, stat_value):
+    
+    try:
+        #If a row for this combination already exists in the stats DataFrame
+        # we update the value of the stat provided        
+        stats_df.loc[(area, res_type, status, org, date), stat_name] = stat_value
+        return stats_df
+        
+    except KeyError:
+        # If there is still no row for this specific combination of GeographicArea, ResourceType, Status, Organization and Date
+        # we insert a new row and set the provided stat with the corresponding value
+        if stat_name == 'NumOfDelegations':
+            num_del = stat_value
+        else:
+            num_del = np.nan
+        
+        if stat_name == 'NumOfResources':
+            num_res = stat_value
+        else:
+            num_res = np.nan
+            
+        if stat_name == 'IPCount':
+            count = stat_value
+        else:
+            count = np.nan
+ 
+        if stat_name == 'NumOfDelegations':
+            space = stat_value
+        else:
+            space = np.nan
+            
+        aux_dic = {'GeographicArea':area,\
+                    'ResourceType':res_type,\
+                    'Status':status,\
+                    'Organization':org,\
+                    'Date':date,\
+                    'NumOfDelegations':num_del,\
+                    'NumOfResources':num_res,\
+                    'IPCount':count,\
+                    'IPSpace':space}
+                    
+        index_cols = ['GeographicArea',
+                 'ResourceType',
+                 'Status',
+                 'Organization',
+                 'Date']
+        stat_cols = ['NumOfDelegations',
+                 'NumOfResources',
+                 'IPCount',
+                 'IPSpace']
+
+        # we provide the column names so that the data frame keeps the order of the columns
+        aux_df = pd.DataFrame(data=aux_dic, columns=index_cols+stat_cols, index=[0])
+        aux_df.set_index(index_cols, inplace=True)
+        stats_df = stats_df.append(aux_df)
+        
+        return stats_df.sort_index()
+    
+def computation_loop(delegated_subset, a, r, s, o, stats_df, dates_range):
                    
     if r == 'All':
         date_groups =\
@@ -130,15 +110,20 @@ def computation_loop(delegated_subset, a, r, d, o, stats_df, dates_range):
                     
                 del_sum = ipv4_del + ipv6_del + asn_del
                 if del_sum > 0:
-                    stats_df.loc[a, r, d, o, date]['NumOfDelegations'] = del_sum
+                    stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                                    'NumOfDelegations', del_sum)
     
                 res_sum = ipv4_del + ipv6_del + asn_res
                 if res_sum > 0:
-                    stats_df.loc[a, r, d, o, date]['NumOfResources'] = res_sum
+                    stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                                        'NumOfResources', res_sum)
                 
             # IPCount and IPSpace do not make sense for r = 'All'
-            stats_df.loc[a, r, d, o, date]['IPCount'] = np.nan
-            stats_df.loc[a, r, d, o, date]['IPSpace'] = np.nan
+            stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                                            'IPCount', np.nan)
+            stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                                            'IPSpace', np.nan)
+
     else: # r != 'All'
         date_groups =\
                 delegated_subset.groupby(delegated_subset['date']\
@@ -168,41 +153,45 @@ def computation_loop(delegated_subset, a, r, d, o, stats_df, dates_range):
         for date in dates_range:
             if r == 'ipv4' or r == 'ipv6':
                 if date in del_nonZeroDates: 
-                    stats_df.loc[a, r, d, o, date]['NumOfDelegations'] =\
-                                                                del_counts[date]
-                    stats_df.loc[a, r, d, o, date]['NumOfResources'] =\
-                                                                del_counts[date]                            
-
+                    stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'NumOfDelegations', del_counts[date])
+                                                                
+                    stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'NumOfResources', del_counts[date])
+                    
                 if r == 'ipv4':
                     if date in ipv4_counts_nonZeroDates:    
                         ipv4_count = ipv4_counts[date]
-                        stats_df.loc[a, r, d, o, date]['IPCount'] =\
-                                                                    ipv4_count
-                        stats_df.loc[a, r, d, o, date]['IPSpace'] =\
-                                                                ipv4_count/256
+                        stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'IPCount', ipv4_count)
+                          
+                        stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'IPSpace', ipv4_count/256)
 
                 else: # r == 'ipv6'
-                    if date in ipv6_counts_nonZeroDates:    
-                        stats_df.loc[a, r, d, o, date]['IPCount'] =\
-                                                                    ipv6_counts[date]
-
+                    if date in ipv6_counts_nonZeroDates:
+                        stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                                    'IPCount', ipv6_counts[date])
+                    
                     if date in ipv6_space_nonZeroDates:    
-                        stats_df.loc[a, r, d, o, date]['IPSpace'] =\
-                                ipv6_space[date]
-
+                        stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                                    'IPSpace', ipv6_space[date])
+                
             else: # r == 'asn'
                 if date in del_nonZeroDates: 
-                    stats_df.loc[a, r, d, o, date]['NumOfDelegations'] =\
-                                                    del_counts[date]
-
+                    stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'NumOfDelegations', del_counts[date])
+        
                 if date in res_nonZeroDates: 
-                    stats_df.loc[a, r, d, o, date]['NumOfResources'] =\
-                                                    res_counts[date]
-
+                    stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'NumOfResources', res_counts[date])
+                    
                 # IPCount and IPSpace do not make sense for r = 'asn'
-                stats_df.loc[a, r, d, o, date]['IPCount'] = float(np.nan)
-                stats_df.loc[a, r, d, o, date]['IPSpace'] = float(np.nan)
-
+                stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'IPCount', np.nan)
+                stats_df = updateOrInsertIntoStatsDF(stats_df, a, r, s, o, date,\
+                                            'IPSpace', np.nan)
+                                            
     return stats_df
     
 def computeStatistics(del_handler, stats_df):
@@ -266,7 +255,7 @@ def computeStatistics(del_handler, stats_df):
                                 continue
                         
                         stats_df = computation_loop(status_res_df, a, r, s, o, stats_df, del_handler.dates_range)
-                    
+                     
     return stats_df
 
 
@@ -291,12 +280,12 @@ def main(argv):
             print "This script computes daily statistics from one of the delegated files provided by the RIRs"
             print 'Usage: delegatd_stats_v3.py -h | -p <files path [-y <year>] [-d <delegated file>] [-e] [-i <stats file>]'
             print 'h = Help'
-            print "p = Path to folder in which files will be saved. (MANDATORY)"
             print 'y = Year to compute statistics for. If a year is not provided, statistics will be computed for all the available years.'
             print 'd = DEBUG mode. Provide path to delegated file. If not in DEBUG mode the latest delegated file will be downloaded from ftp://ftp.apnic.net/pub/stats/apnic'
             print 'e = Use Extended file'
             print "If option -e is used in DEBUG mode, delegated file must be a extended file."
             print "If option -e is not used in DEBUG mode, delegated file must be delegated file not extended."
+            print "p = Path to folder in which files will be saved. (MANDATORY)"
             print "i = Incremental. Compute incremental statistics from existing stats file (CSV)."
             print "If option -i is used, a statistics file MUST be provided."
             sys.exit()
@@ -312,8 +301,6 @@ def main(argv):
         elif opt == '-i':
             INCREMENTAL = True
             stats_file = arg
-        elif opt == '-t':
-            stat = arg
         else:
             assert False, 'Unhandled option'
 
@@ -329,7 +316,19 @@ def main(argv):
     if stat != '' and stat not in statistics:
         print "Wrong statistic provided."
         sys.exit()
-               
+
+    index_cols = ['GeographicArea',
+                 'ResourceType',
+                 'Status',
+                 'Organization',
+                 'Date']
+    
+    stat_cols = ['NumOfDelegations',
+                 'NumOfResources',
+                 'IPCount',
+                 'IPSpace']
+        
+        
     
     if INCREMENTAL:
         existing_stats_df = pd.DataFrame()
@@ -356,13 +355,10 @@ def main(argv):
     del_handler = DelegatedHandler(DEBUG, EXTENDED, del_file, INCREMENTAL, final_existing_date, year)
         
     if not del_handler.delegated_df.empty:
-        stats_df = initializeStatsDF(del_handler, EXTENDED, stat)
+        stats_df = pd.DataFrame(columns=index_cols+stat_cols)
+        stats_df.set_index(index_cols, inplace=True)
         sys.stderr.write("Stats Data Frame initialized successfully!\n")
         stats_df = computeStatistics(del_handler, stats_df)
-        # We remove the rows with more than 2 nan values
-        # The rows for which IPSpace and IPCount don't make sense have 2 nan values
-        # and we want te keep those
-        stats_df = stats_df.dropna(thresh=2)
         
         if INCREMENTAL:
             stats_df = pd.concat([existing_stats_df, stats_df])