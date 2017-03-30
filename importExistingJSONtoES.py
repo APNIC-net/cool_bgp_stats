8df5aedad1e4db9bae0eac28ba0956102c9861e5
Sofia <sofia@apnic.net>
2017-03-30 16:11:19 -0300

    Fixes in importExistingJSONtoES

--- importExistingJSONtoES.py
+++ importExistingJSONtoES.py
@@ -6,16 +6,17 @@ Created on Mon Feb 20 13:46:12 2017
 """
 
 import sys, getopt, os
-import hashlib
 import pandas as pd
-import elasticsearch
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+from elasticsearch import exceptions
 
 def connect(host):
     # configure elasticsearch
 	config = {
          'host': host
 	}
-	return elasticsearch.Elasticsearch([config,], timeout=300)
+	return Elasticsearch([config,], timeout=300)
 
 def createIndex(es, mapping_dict, index_name):
     request_body = {
@@ -25,62 +26,47 @@ def createIndex(es, mapping_dict, index_name):
                          },                
                     "mappings": mapping_dict
                     }
-
+                    
     print("creating {} index...".format(index_name))
     es.indices.create(index = index_name, body = request_body, ignore=400)
     es.indices.refresh(index = index_name)
 
-def prepareData(data_for_es, index_name, doc_type, id_column):
+def prepareData(data_for_es, index_name, doc_type, numOfDocs):
     bulk_data = []
-
+    
     for index, row in data_for_es.iterrows():
         data_dict = {}
+
         for i in range(len(row)):
             data_dict[data_for_es.columns[i]] = row[i]
 
         op_dict = {
-            "index": {
+                "_op_type": "index",
                 "_index": index_name,
                 "_type": doc_type,
-                "_id": data_dict[id_column]
-                    }
-                    }
+                "_id": numOfDocs,
+                "_source": data_dict
+                    }  
+
+        numOfDocs += 1
+
         bulk_data.append(op_dict)
-        bulk_data.append(data_dict)
-    
-    return bulk_data, len(data_for_es)
+
+    return bulk_data, numOfDocs
      
 def inputData(es, index_name, bulk_data, numOfDocs):
-    es.bulk(index = index_name, body = bulk_data, refresh = True)
+    helpers.bulk(es, bulk_data)
 
     # check data is in there, and structure in there
     try:
-        res = es.search(body={"query": {"match_all": {}}}, index = index_name)
         es.indices.get_mapping(index = index_name)
-        if len(res['hits']['hits']) == numOfDocs:
+        if es.count('delegated_stats_index') == numOfDocs:
             return True
         else:
             return False
-    except elasticsearch.exceptions.NotFoundError:
+    except exceptions.NotFoundError:
         return False
  
-
-def hashFromColValue(col_value):
-    return hashlib.md5(col_value).hexdigest()
-    
-def preparePlainDF(plain_df):
-    plain_df['GeographicArea'] = plain_df['Geographic Area']
-    del plain_df['Geographic Area']
-    
-    plain_df['multiindex_comb'] = plain_df['GeographicArea'] +\
-                                    plain_df['ResourceType'] +\
-                                    plain_df['Status'] +\
-                                    plain_df['Organization']
-                                    
-    plain_df['index'] = plain_df['multiindex_comb'].apply(hashFromColValue)
-    plain_df['del_stat_id'] = plain_df['Date'].astype('str') + '_' + plain_df['index'].astype('str')
-    del plain_df['index']
-    del plain_df['multiindex_comb']
     
 def main(argv):    
 
@@ -88,9 +74,7 @@ def main(argv):
     host = ''
 #    user = ''
 #    password = ''
-    
-    id_column = 'del_stat_id'
-    
+        
     # Mapping for stats about delegations
     delStats_mapping = {"_default_" : {
                             "properties" : {
@@ -144,7 +128,7 @@ def main(argv):
                                         }
                                     }
                                 }
-    del_stats_index_name = 'delegated_stats'
+    del_stats_index_name = 'delegated_stats_index'
     del_stats_doc_type = 'delegated_stats'
     
     try:
@@ -176,24 +160,19 @@ def main(argv):
     if host != '':
         es = connect(host)
         createIndex(es, delStats_mapping, del_stats_index_name)
+        existingDocs = es.search(body={"query": {"match_all": {}}}, index = del_stats_index_name)
+        numOfDocs = len(existingDocs['hits']['hits'])
+                
         for json_file in os.listdir(files_path):
             if json_file.endswith(".json"):
                 plain_df = pd.read_json('%s/%s' % (files_path, json_file), orient = 'index').reset_index()
-                del plain_df['index']
-                # Originally stats were oomputed considering Org = All,
-                # ResourceType = All, Status = All, GeographicArea = All
-                # However, this is not necessary as it contains redundant info
-                # and makes it harder to filter the stats when analyzing
-                # or visualizing them
-                # So we remove these rows of the Data Frame
-                plain_df = plain_df[plain_df['Organization'] != 'All']
-                plain_df = plain_df[plain_df['Geographic Area'] != 'All']
-                plain_df = plain_df[plain_df['ResourceType'] != 'All']
-                plain_df = plain_df[plain_df['Status'] != 'All']
-                
-                preparePlainDF(plain_df)
-                bulk_data, numOfDocs = prepareData(plain_df, del_stats_index_name,\
-                                                    del_stats_doc_type, id_column)
+                plain_df['GeographicArea'] = plain_df['Geographic Area']
+                del plain_df['Geographic Area']
+                plain_df = plain_df.fillna(-1)
+
+                bulk_data, numOfDocs = prepareData(plain_df, del_stats_index_name,
+                                                    del_stats_doc_type, numOfDocs)
+                                                    
                 dataImported = inputData(es, del_stats_index_name, bulk_data, numOfDocs)
 
                 if dataImported:
