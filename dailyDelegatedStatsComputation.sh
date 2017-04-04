3fb19c1f05ecbdbe1fcf1bd16d34c23bea381682
Sofia <sofia@apnic.net>
2017-04-03 21:19:52 -0300

    dailyDelegatedStatsComputation.sh updated according to changes in delegated_stats_v5.py

--- dailyDelegatedStatsComputation.sh
+++ dailyDelegatedStatsComputation.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/local/bin/bash
+
 # This script receives the path to the python script that computes statistics about delegations as first argument
 # and the path to the folder where already computed statistics are stored.
 # This scrips lists the provided folder looking for a csv file with stats about delegations for the current year
@@ -10,19 +11,19 @@ pythonScript=$1
 delStats_folder=$2
 
 curr_year=$(date +'%Y')
-statsForCurrYear=`ls $delStats_folder | grep "_$curr_year" | grep csv`
+statsForCurrYear=($(ls $delStats_folder | grep "_$curr_year" | grep csv))
 numOfStatsFiles=${#statsForCurrYear[@]}
 case $numOfStatsFiles in
-	0) echo "No file with stats about delegations for current year found."
-	echo "Computing stats for the whole year"
-	python $pythonScript -p $delStats_folder -e -D $curr_year
-	;;
-	1) echo "File with stats about delegations for current year found."
-	echo $delStats_folder/$statsForCurrYear
-	echo "Computing stats for the rest of the year."
-	python $pythonScript -p $delStats_folder -e -i $delStats_folder/$statsForCurrYear -D $curr_year
-	;;
-	*) echo "More than one file with stats about delegations for current year found!"
-	exit
-	;;
+        0) echo "No file with stats about delegations for current year found."
+        echo "Computing stats for the whole year"
+        python $pythonScript -p $delStats_folder -e -S $curr_year -E $curr_year -H twerp.rand.apnic.net
+        ;;
+        1) echo "File with stats about delegations for current year found."
+        echo $delStats_folder/$statsForCurrYear
+        echo "Computing stats for the rest of the year."
+        python $pythonScript -p $delStats_folder -e -i $delStats_folder/$statsForCurrYear -S $curr_year -E $curr_year -H twerp.rand.apnic.net
+        ;;
+        *) echo "More than one file with stats about delegations for current year found!"
+        exit
+        ;;
 esac
