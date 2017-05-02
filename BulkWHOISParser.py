1578c6f424987bef99fa2e83cf85fc7f8a94bd32
Sofia <sofia@apnic.net>
2017-05-02 16:23:04 +0200

    BulkWHOISParser updated
    * Code related to as-block removed as apnic.db.as-block does not
    contain any useful info for the heuristics.
    * ‘address’ field removed as it is too hard to compare and would
    probable lead to false positives and false negatives.

--- BulkWHOISParser.py
+++ BulkWHOISParser.py
@@ -13,108 +13,6 @@ import radix
 import ipaddress
 import pickle
 
-'''
-https://ftp.apnic.net/apnic/whois/apnic.db.inetnum.gz
-inetnum:      202.6.91.0 - 202.6.91.255
-netname:      NLA
-country:      AU
-descr:        National Library of Australia
-descr:        Parkes Place
-descr:        Canberra  ACT 2600
-admin-c:      NC68-AP
-tech-c:       NC68-AP
-status:       ASSIGNED PORTABLE
-mnt-by:       APNIC-HM
-mnt-routes:   MAINT-AU-NLA
-source:       APNIC
-
-https://ftp.apnic.net/apnic/whois/apnic.db.inet6num.gz
-inet6num:     2001:200:600::/40
-netname:      KDDLABS
-descr:        KDD Labs IPv6 Network
-country:      JP
-admin-c:      TA19-AP
-tech-c:       MF29-AP
-tech-c:       AK44-AP
-tech-c:       YH48-AP
-status:       ASSIGNED NON-PORTABLE
-remarks:      tunnel to otemachi-pop
-mnt-by:       MAINT-JP-WIDE
-source:       APNIC
-
-https://ftp.apnic.net/apnic/whois/apnic.db.aut-num.gz
-aut-num:        AS23553
-as-name:        IITA-AS-KR
-descr:          National IT Industry Promotion Agency
-country:        KR
-admin-c:        AM1923-AP
-tech-c:         AM1923-AP
-import:         from AS4766
-                action pref=10;
-                accept ANY
-import:         from AS9318
-                action pref=100;
-                accept ANY
-export:         to AS4766
-                announce AS23553
-export:         to AS9318
-                announce AS23553
-default:        to AS4766
-                action pref=10;
-                networks ANY
-mnt-by:         MNT-KRNIC-AP
-mnt-irt:        IRT-KRNIC-KR
-source:         APNIC
-
-
-https://ftp.apnic.net/apnic/whois/apnic.db.as-block.gz
-as-block:     AS23612 - AS23643
-descr:        JPNIC-2Byte-ASBLOCK-AP
-descr:        for assignment to JPNIC members
-country:      JP
-admin-c:      JNIC1-AP
-tech-c:       JNIC1-AP
-remarks:      Authoritative information regarding AS Number
-remarks:      assignments made from within this block can be
-remarks:      queried at whois.nic.ad.jp.  To only display English output,
-remarks:      add '/e' at the end of command,
-remarks:      e.g. 'whois -h whois.nic.ad.jp xxx/e'.
-mnt-by:       MAINT-JPNIC
-source:       APNIC
-
-
-https://ftp.apnic.net/apnic/whois/apnic.db.mntner.gz
-mntner:       APNIC-HM
-descr:        Maintainer for allocations/assignments made by APNIC
-descr:        Objects maintained by this maintainer may
-descr:        only be changed by APNIC Hostmasters.
-admin-c:      HM20-AP
-tech-c:       NO4-AP
-upd-to:       ********
-mnt-by:       APNIC-HM
-referral-by:  APNIC-HM
-source:       APNIC
-
-
-https://ftp.apnic.net/apnic/whois/apnic.db.irt.gz
-irt:            irt-CERT-NURUL
-address:        6 Cordelia Street
-address:        South Brisbane
-address:        QLD 4101
-phone:          +61738583100
-fax-no:         +61738583199
-abuse-mailbox:  ********
-e-mail:         ********
-signature:      PGPKEY-D7F5AA55
-encryption:     PGPKEY-D7F5AA55
-admin-c:        NR97-AP
-tech-c:         NR97-AP
-remarks:        CERT-NURUL is the CRT Team of Nurul
-irt-nfy:        ********
-mnt-by:         MAINT-NEW
-source:         APNIC
-'''
-
 class BulkWHOISParser:
     whois_data = dict()
 
@@ -123,26 +21,20 @@ class BulkWHOISParser:
     whois_data['inetnum']['filename'] = 'apnic.db.inetnum'
     whois_data['inetnum']['fields'] = {'admin-c:', 'descr:', 'mnt-by:', 'mnt-irt:',
                                          'mnt-lower:', 'mnt-routes:', 'netname:',
-                                         'remarks:', 'tech-c:'}
+                                         'remarks:', 'tech-c:', 'status:'}
     
     whois_data['inet6num'] = dict()
     whois_data['inet6num']['url'] = 'https://ftp.apnic.net/apnic/whois/apnic.db.inet6num.gz'
     whois_data['inet6num']['filename'] = 'apnic.db.inet6num'
     whois_data['inet6num']['fields'] = {'admin-c:', 'descr:', 'mnt-by:', 'mnt-irt:',
                                          'mnt-lower:', 'mnt-routes:', 'netname:',
-                                         'remarks:', 'tech-c:'}
+                                         'remarks:', 'tech-c:', 'status:'}
     
     whois_data['aut-num'] = dict()
     whois_data['aut-num']['url'] = 'https://ftp.apnic.net/apnic/whois/apnic.db.aut-num.gz'
     whois_data['aut-num']['filename'] = 'apnic.db.aut-num'
     whois_data['aut-num']['fields'] = {'admin-c:', 'as-name:', 'descr:', 'mnt-by:',
                                         'mnt-irt:', 'mnt-lower:', 'mnt-routes:',
-                                        'remarks:', 'tech-c:'}
-
-    whois_data['as-block'] = dict()
-    whois_data['as-block']['url'] = 'https://ftp.apnic.net/apnic/whois/apnic.db.as-block.gz'
-    whois_data['as-block']['filename'] = 'apnic.db.as-block'
-    whois_data['as-block']['fields'] = {'admin-c:', 'descr:', 'mnt-by:', 'mnt-lower:',
                                         'remarks:', 'tech-c:'}
 
     whois_data['mntner'] = dict()
@@ -154,7 +46,7 @@ class BulkWHOISParser:
     whois_data['irt'] = dict()
     whois_data['irt']['url'] = 'https://ftp.apnic.net/apnic/whois/apnic.db.irt.gz'
     whois_data['irt']['filename'] = 'apnic.db.irt'
-    whois_data['irt']['fields'] = {'address:', 'admin-c:', 'fax-no:', 'mnt-by:',
+    whois_data['irt']['fields'] = {'admin-c:', 'fax-no:', 'mnt-by:',
                                     'phone:', 'remarks:', 'tech-c:'}
 
     
@@ -180,8 +72,6 @@ class BulkWHOISParser:
         for item in self.whois_data:
             if item == 'inetnum' or item == 'inet6num':
                 current_structure = radix.Radix()
-            elif item == 'as-block':
-                current_structure = RangeDict()
             else:
                 current_structure = dict()
                 
@@ -234,10 +124,6 @@ class BulkWHOISParser:
             current_structure[asn] = current_dict
             asn = -1
             
-        elif elementType == 'as-block':
-            current_structure[as_block] = current_dict
-            as_block = None
-            
         else:
             current_structure[contact_id] = current_dict
             contact_id = ''
@@ -286,16 +172,6 @@ class BulkWHOISParser:
                             
                     asn = int(line_data[2:])
     
-                elif item == 'as-block':
-                    if as_block is not None and len(current_dict) > 0:
-                        ip_blocks, asn, as_block, contact_id =\
-                            self.insertElement(item, current_structure, current_dict,
-                                               ip_blocks, asn, as_block, contact_id)
-                        current_dict = dict()
-                            
-                    as_block_parts = line_data.split('-')
-                    as_block = xrange(int(as_block_parts[0].strip()[2:]), int(as_block_parts[1].strip()[2:])+1)
-                    
                 else: # item = 'mntner' or item = 'irt'
                     if contact_id != '' and len(current_dict) > 0:
                         ip_blocks, asn, as_block, contact_id =\
@@ -319,20 +195,13 @@ class BulkWHOISParser:
 
         return ip_blocks, asn, as_block, contact_id, current_dict
 
-class RangeDict(dict):
-    def __getitem__(self, item):
-        if type(item) != xrange:
-            dict_list = []
-            for key in self:
-                if item in key:
-                    dict_list.append(self[key])
-            return dict_list
-        else:
-            return super(RangeDict, self).__getitem__(item)
-
 def main(argv):
     files_path = ''
     DEBUG = False
+    
+    #For DEBUG
+    DEBUG = True
+    files_path = '/Users/sofiasilva/Downloads'
     
     try:
         opts, args = getopt.getopt(argv, 'hf:D', ['files_path='])
@@ -347,7 +216,7 @@ def main(argv):
             print 'h = Help'
             print "f = Path to folder in which files will be saved. (MANDATORY)"
             print "D = DEBUG. If this option is used bulk WHOIS files will not be downloaded."
-            print "In DEBUG mode the bulk WHOIS files: apnic.db.inetnum, apnic.db.inet6num, apnic.db.aut-num, apnic.db.as-block, apnic.db.mntner and apnic.db.irt MUST be already present in the files_path folder."
+            print "In DEBUG mode the bulk WHOIS files: apnic.db.inetnum, apnic.db.inet6num, apnic.db.aut-num, apnic.db.mntner and apnic.db.irt MUST be already present in the files_path folder."
         elif opt == '-f':
             files_path = os.path.abspath(arg)
         elif opt == '-D':
