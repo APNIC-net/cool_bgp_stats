cd19b065d2ada77de475da2346fc777f68b594ad
Sofia <sofia@apnic.net>
2017-06-06 17:02:37 -0300

    Organized code and created main. Ready for automatization

--- StabilityAndDeaggDailyStats.py
+++ StabilityAndDeaggDailyStats.py
@@ -19,70 +19,145 @@ import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 #Just for DEBUG
 #os.chdir('/Users/sofiasilva/GitHub/cool_bgp_stats')
+import sys, getopt
 from BGPDataHandler import BGPDataHandler
 from netaddr import IPNetwork
 import pandas as pd
 import subprocess, shlex
 from datetime import datetime
 
-DEBUG = True
-files_path = '/Users/sofiasilva/BGP_files'
- 
-routing_file = '/Users/sofiasilva/BGP_files/2017-05-01.bgprib.readable' 
-updates_file = '/Users/sofiasilva/BGP_files/2017-05-01_test.bgpupd.readable'
- 
-bgp_handler = BGPDataHandler(DEBUG, files_path)
-
-#readable_updates = bgp_handler.getReadableFile(updates_file, False)
-readable_updates = updates_file
-bgp_handler.loadStructuresFromUpdatesFile(updates_file)
-
-updateRates_DF = pd.DataFrame(columns=['updates_date', 'ip_version', 'prefLength',
-                                       'numOfAnnouncements', 'numOfWithdraws'])
-
-for prefLength, prefLength_subset in bgp_handler.updates_df.groupby('prefLength'):
-    for ipVersion, ipver_subset in prefLength_subset.groupby('ip_version'):
-        for routing_date, date_subset in ipver_subset.groupby('updates_date'):
-            updateRates_DF.loc[updateRates_DF.shape[0]] = [routing_date,
-                                                            ipVersion,
-                                                            prefLength,
-                                                            sum(date_subset['numOfAnnouncements']),
-                                                            sum(date_subset['numOfWithdraws'])]
-
-bgp_handler.loadStructuresFromRoutingFile(routing_file)
-
-deaggregation_DF = pd.DataFrame(columns=['prefix', 'del_date', 'routing_date',
-                                          'isRoot', 'isRootDeagg'])
-
-cmd = shlex.split("origindate -d ',' -f 1")
-
-for prefix, prefix_subset in bgp_handler.bgp_df.groupby('prefix'):
+def getDelegationDate(prefix):
+    cmd = shlex.split("origindate -d ',' -f 1")
     cmd_echo = shlex.split("echo {}".format(prefix))
     p = subprocess.Popen(cmd_echo, stdout=subprocess.PIPE)
     output = subprocess.check_output(cmd, stdin=p.stdout)
-    del_date = datetime.strptime(output.split(',')[1], '%Y%m%d').date()
+    return datetime.strptime(output.split(',')[1], '%Y%m%d').date()
     
-    network = IPNetwork(prefix)
-    if network.version == 4:
-        prefixes_radix = bgp_handler.ipv4Prefixes_radix
-    else:
-        prefixes_radix = bgp_handler.ipv6Prefixes_radix
+def getUpdatesCountsDF(updates_df):
+    updates_df['update_date'] = updates_df.apply(lambda row:\
+                                            datetime.utcfromtimestamp(\
+                                            row['timestamp']).date(),\
+                                            axis=1)
+                                                
+    updates_counts_df = pd.DataFrame(columns=['prefix', 'del_date', 'updates_date',
+                                             'del_age', 'ip_version', 'prefLength',
+                                             'numOfAnnouncements', 'numOfWithdraws'])
+                                             
+    for prefix, prefix_subset in updates_df.groupby('prefix'):
+        del_date = getDelegationDate(prefix)
+        network = IPNetwork(prefix)
+        for update_date, date_subset in prefix_subset.groupby('update_date'):
+            del_age = (update_date - del_date).days
+            numOfAnn = len(date_subset[date_subset['upd_type'] == 'A']['prefix'].tolist())
+            numOfWith = len(date_subset[date_subset['upd_type'] == 'W']['prefix'].tolist())
+            
+            updates_counts_df.loc[updates_counts_df.shape[0]] = [prefix, del_date,
+                                                                update_date, del_age,
+                                                                network.version,
+                                                                network.prefixlen,
+                                                                numOfAnn, numOfWith]
+    return updates_counts_df
+
+def getDeaggregationDF(bgp_handler):
+    deaggregation_DF = pd.DataFrame(columns=['prefix', 'del_date', 'routing_date',
+                                             'del_age', 'isRoot', 'isRootDeagg'])
+    
+    for prefix, prefix_subset in bgp_handler.bgp_df.groupby('prefix'):
+        del_date = getDelegationDate(prefix)
         
-    # If the list of covering prefixes in the Radix tree has only 1 prefix,
-    # it is the prefix itself, therefore the prefix is a root prefix
-    if len(prefixes_radix.search_covering(prefix)) == 1:
-        isRoot = True
-
-        # If the list of covered prefix includes more prefixes than the prefix
-        # itself, then the root prefix is being deaggregated.
-        if len(bgp_handler.ipv4Prefixes_radix.search_covered(prefix)) > 1:
-            isRootDeagg = True
-    else:
-        isRoot = False
-        isRootDeagg = False
+        network = IPNetwork(prefix)
+        if network.version == 4:
+            prefixes_radix = bgp_handler.ipv4Prefixes_radix
+        else:
+            prefixes_radix = bgp_handler.ipv6Prefixes_radix
+            
+        # If the list of covering prefixes in the Radix tree has only 1 prefix,
+        # it is the prefix itself, therefore the prefix is a root prefix
+        if len(prefixes_radix.search_covering(prefix)) == 1:
+            isRoot = True
     
-    deaggregation_DF.iloc[deaggregation_DF.shape[0]] = [prefix, del_date,
-                                                        bgp_handler.routingDate,
-                                                        isRoot, isRootDeagg]
+            # If the list of covered prefix includes more prefixes than the prefix
+            # itself, then the root prefix is being deaggregated.
+            if len(bgp_handler.ipv4Prefixes_radix.search_covered(prefix)) > 1:
+                isRootDeagg = True
+        else:
+            isRoot = False
+            isRootDeagg = False
+        
+        deaggregation_DF.loc[deaggregation_DF.shape[0]] = [prefix, del_date,
+                                                            bgp_handler.routingDate,
+                                                            (bgp_handler.routingDate -\
+                                                            del_date).days,
+                                                            isRoot, isRootDeagg]
+    
+    return deaggregation_DF
 
-# TODO DEBUG
+def main(argv):
+    DEBUG = False
+    files_path = ''
+    routing_file = ''
+    updates_file = ''
+    
+    # For DEBUG
+    DEBUG = True
+    files_path = '/Users/sofiasilva/BGP_files'
+    routing_file = '/Users/sofiasilva/BGP_files/2017-05-01.bgprib.readable' 
+    updates_file = '/Users/sofiasilva/BGP_files/2017-05-01_test.bgpupd.readable'
+ 
+    
+    try:
+        opts, args = getopt.getopt(argv, "hp:D", ["files_path="])
+    except getopt.GetoptError:
+        print 'Usage: {} -h | -p <files path> -R <Routing file> -U <BGP Updates file> [-D]'.format(sys.argv[0])
+        sys.exit(-1)
+        
+    for opt, arg in opts:
+        if opt == '-h':
+            print "This script loads two tables with data about daily BGP updates and deaggregation in the BGP routing table."
+            print 'Usage: {} -h | -p <files path> -R <Routing file> -U <BGP Updates file> [-D]'.format(sys.argv[0])
+            print "h: Help"
+            print "p: Path to folder in which files will be saved. (MANDATORY)"
+            print "R: Path to the routing file to be used. Can be an mrt or a dmp file. Can be a compressed file with extension .gz (MANDATORY)"
+            print "U: Path to the file with BGP updates to be used. Must be a bgpupd.mrt file. Can be a compressed file with extension .gz (MANDATORY)"
+            print "D: Debug mode. Use this option if you want to script to run in debug mode."
+        elif opt == '-p':
+            if arg != '':
+                files_path = os.path.abspath(arg)
+            else:
+                print "If option -p is used, the path to a folder in which files will be saved MUST be provided."
+                sys.exit(-1)
+        elif opt == '-R':
+            if arg != '':
+                routing_file = os.path.abspath(arg)
+            else:
+                print "If option -R is used, the path to the routing file to be used MUST be provided."
+                sys.exit(-1)
+        elif opt == '-U':
+            if arg != '':
+                updates_file = os.path.abspath(arg)
+            else:
+                print "If option -U is used, the path to the file with BGP updates to be used MUST be provided."
+                sys.exit(-1)
+        elif opt == '-D':
+            DEBUG = True
+        else:
+            assert False, 'Unhandled option'
+    
+    if files_path == '' or routing_file == '' or updates_file == '':
+        print "You MUST provide the corresponding paths using options -p, -R and -U."
+        sys.exit(-1)
+        
+    bgp_handler = BGPDataHandler(DEBUG, files_path)
+
+    bgp_handler.loadStructuresFromUpdatesFile(updates_file)
+    updates_count_df = getUpdatesCountsDF(bgp_handler.updates_df)
+    
+    bgp_handler.loadStructuresFromRoutingFile(routing_file)
+    deaggregation_df = getDeaggregationDF(bgp_handler)
+    
+    # TODO Generate csv and json files
+    # TODO Insert both DataFrames into ElasticSearch for aggregation and summarization (compute annual averages)
+        
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
