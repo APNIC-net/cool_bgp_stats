88fb71d8c5ebb78f7bd41550005d68e360090588
Sofia <sofia@apnic.net>
2017-06-16 14:10:23 -0300

    Minor fix

--- bgp_rib.py
+++ bgp_rib.py
@@ -422,7 +422,7 @@ class BGPRIB(dict):
                             if len(line_parts) > 1 and len(line_parts[1]) > 18:
                                 field_parts = line_parts[1].split('/')
                                 network = '{}/{}'.format(field_parts[0], field_parts[1][0:2])
-                                nexthop = field_parts[2:]
+                                nexthop = field_parts[1][2:]
                             else:
                                 if len(line_parts) > 1:
                                     network = line_parts[1]
