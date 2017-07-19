906855763601419c0422b72c05d7422c32fe2193
Sofia <sofia@apnic.net>
2017-07-19 19:53:23 +0200

    Updated to insert routing data from the archive folder for today and visibility data for yesterday (as by-rib.mrt file may have not been generated yet)

--- dailyInsertion.py
+++ dailyInsertion.py
@@ -31,9 +31,7 @@ def insertionForDate(date_to_work_with):
     sys.stdout.write('{}: Starting generating CSV files for insertion of visibility, routing and updates data into the DB.\n'.format(datetime.now()))
     
     bgp_handler = BGPDataHandler(DEBUG, files_path)
-    
-    routing_files = dict()
-    
+        
     file_name = '{}/{}/{}/{}/{}-{}-{}'.format(archive_folder,
                                                     date_to_work_with.year,
                                                     date_to_work_with.strftime('%m'),
@@ -42,19 +40,10 @@ def insertionForDate(date_to_work_with):
                                                     date_to_work_with.strftime('%m'),
                                                     date_to_work_with.strftime('%d'))
     
+    # We first insert the files available in the current folder of the archive
+    # into the routing_data table
     bgprib_file = '{}.bgprib.mrt'.format(file_name)
-    
-    if os.path.exists(bgprib_file):
-        routing_files['bgprib'] = bgprib_file
-        
-        dates_ready, visibility_csvs = generateFilesFromRoutingFile(files_path,
-                                                                   bgprib_file,
-                                                                   bgp_handler,
-                                                                   'visibility',
-                                                                   dict(), log_file,
-                                                                   archive_folder,
-                                                                   DEBUG)
-        
+    if os.path.exists(bgprib_file):       
         dates_ready, routing_bgprib_csv = generateFilesFromRoutingFile(files_path,
                                                                        bgprib_file,
                                                                        bgp_handler,
@@ -64,10 +53,7 @@ def insertionForDate(date_to_work_with):
                                                                        DEBUG)    
     
     dmp_file = '{}.dmp.gz'.format(file_name)
-    
     if os.path.exists(dmp_file):
-        routing_files['dmp'] = dmp_file
-        
         dates_ready, routing_dmp_csv = generateFilesFromRoutingFile(files_path,
                                                                     dmp_file,
                                                                     bgp_handler,
@@ -77,9 +63,7 @@ def insertionForDate(date_to_work_with):
                                                                     DEBUG)
     
     v6dmp_file = '{}.v6.dmp.gz'.format(file_name)
-    
     if os.path.exists(v6dmp_file):
-        routing_files['v6.dmp'] = v6dmp_file
         dates_ready, routing_v6dmp_csv = generateFilesFromRoutingFile(files_path,
                                                                       v6dmp_file,
                                                                       bgp_handler,
@@ -88,57 +72,30 @@ def insertionForDate(date_to_work_with):
                                                                       log_file,
                                                                       archive_folder,
                                                                       DEBUG)
+
+    # For visibility data, we insert data about the day before because the mrt file is generated the day after
+    day_before = date_to_work_with - timedelta(1)
     
-    if 'bgprib' not in routing_files:
-        readable_v4 = ''
-        if 'dmp' in routing_files:
-            readable_v4 = BGPDataHandler.getReadableFile(routing_files['dmp'],
-                                                         False, files_path, DEBUG)
-        
-        readable_v6 = ''
-        if 'v6.dmp' in routing_files:
-            readable_v6 = BGPDataHandler.getReadableFile(routing_files['v6.dmp'],
-                                                         False, files_path, DEBUG)
-        readable_complete = ''
-                         
-        if readable_v4 != '' and readable_v6 != '':
-            readable_complete = concatenateFiles('{}/routing_file_{}.dmp.readable'\
-                                                    .format(date_to_work_with),
-                                                 readable_v4, readable_v6)
-            os.remove(readable_v4)
-            os.remove(readable_v6)
-            
-        elif readable_v4 != '':
-            readable_complete = readable_v4
-        elif readable_v6 != '':
-            readable_complete = readable_v6
-    
-        if readable_complete != '':
-           dates_ready, visibility_csvs = generateFilesFromRoutingFile(files_path,
-                                                                       readable_complete,
-                                                                       bgp_handler,
-                                                                       'visibility',
-                                                                       dict(), log_file,
-                                                                       archive_folder,
-                                                                       DEBUG)
-                                                                       
-    else:
-        readable_complete = '{}/{}-{}-{}.bgprib.readable'.format(files_path,
-                                                                date_to_work_with.year,
-                                                                date_to_work_with.strftime('%m'),
-                                                                date_to_work_with.strftime('%d'))
-                                                                       
+    routing_file = BGPDataHandler.getRoutingFileForDate(day_before)
+
+    dates_ready, visibility_csvs = generateFilesFromRoutingFile(files_path,
+                                                               routing_file,
+                                                               bgp_handler,
+                                                               'visibility',
+                                                               dict(), log_file,
+                                                               archive_folder,
+                                                               DEBUG)
+                                                               
     updates_file = '{}.bgpupd.mrt'.format(file_name)
     
     if not os.path.exists(updates_file):
-        date_for_log = date_to_work_with - timedelta(1)
         updates_file = '{}/{}/{}/{}/{}-{}-{}.log.gz'.format(archive_folder,
-                                                            date_for_log.year,
-                                                            date_for_log.strftime('%m'),
-                                                            date_for_log.strftime('%d'),
-                                                            date_for_log.year,
-                                                            date_for_log.strftime('%m'),
-                                                            date_for_log.strftime('%d'))
+                                                            day_before.year,
+                                                            day_before.strftime('%m'),
+                                                            day_before.strftime('%d'),
+                                                            day_before.year,
+                                                            day_before.strftime('%m'),
+                                                            day_before.strftime('%d'))
 
     updates_csv = generateCSVFromUpdatesFile(updates_file, files_path, files_path,
                                              DEBUG, log_file)
@@ -155,18 +112,20 @@ def insertionForDate(date_to_work_with):
         sql_f.write("copy routing_data (routing_date, extension, file_path) from '{}' WITH (FORMAT csv);\n".format(routing_bgprib_csv[0]))
         sql_f.write("copy routing_data (routing_date, extension, file_path) from '{}' WITH (FORMAT csv);\n".format(routing_dmp_csv[0]))
         sql_f.write("copy routing_data (routing_date, extension, file_path) from '{}' WITH (FORMAT csv);\n".format(routing_v6dmp_csv[0]))
-        # If it is the first day of the year
-        if date_to_work_with.month == 1 and date_to_work_with.day == 1:
+        # If day_before is the first day of the year
+        # (we consider day_beofre because the bgpupd.mrt file contains data for
+        # the day before the date in its name)
+        if day_before.month == 1 and day_before.day == 1:
             # We need to create the partition for the new year and its indexes
             sql_f.write('''CREATE TABLE updates_y{} (
                             CHECK ( update_date >= DATE '{}-01-01' AND 
                             update_date <= DATE '{}-12-31' )
-                            ) INHERITS (updates);\n'''.format(date_to_work_with.year,
-                                                            date_to_work_with.year))
+                            ) INHERITS (updates);\n'''.format(day_before.year,
+                                                            day_before.year))
                                                             
             sql_f.write('''CREATE INDEX updates_y{}_update_date 
                             ON updates_y{} (update_date);\n'''\
-                            .format(date_to_work_with.year))
+                            .format(day_before.year))
                             
             # and we need to update the updates_insert_trigger function
             sql_f.write('''create or replace function updates_insert_trigger()\n
@@ -176,10 +135,10 @@ def insertionForDate(date_to_work_with):
             sql_f.write('''IF ( NEW.update_date >= DATE '{}-01-01' 
                             AND NEW.update_date <= '{}-12-31' ) 
                             THEN INSERT INTO updates_y{} VALUES (NEW.*);\n'''\
-                            .format(date_to_work_with.year, date_to_work_with.year,
-                                    date_to_work_with.year))
+                            .format(day_before.year, day_before.year,
+                                    day_before.year))
                                     
-            for year in range(date_to_work_with.year-1, 2006, -1):
+            for year in range(day_before.year-1, 2006, -1):
                 sql_f.write('''ELSIF ( NEW.update_date >= DATE '{}-01-01' 
                                 AND NEW.update_date <= '{}-12-31' ) 
                                 THEN INSERT INTO updates_y{} VALUES (NEW.*);\n'''\
@@ -194,12 +153,12 @@ def insertionForDate(date_to_work_with):
                             $$\n
                             LANGUAGE plpgsql;\n''')
     
-        # If the CSV file corresponds to the first day of the year or to
-        # the last day of the year, we insert into the master table (updates table)
+        # If day_before is the first or the last day of the year,
+        # we insert into the master table (updates table)
         # and let the updates_insert_trigger function redirect the insertions
         # to the right partition, as the CSV file may contain rows for a different year.
-        if date_to_work_with.month == 1 and date_to_work_with.day == 1 or\
-            date_to_work_with.month == 12 and date_to_work_with.day == 31:
+        if day_before.month == 1 and day_before.day == 1 or\
+            day_before.month == 12 and day_before.day == 31:
     
             sql_f.write('''copy updates (update_date, update_time, upd_type, 
                                     bgp_neighbor, peeras, prefix, source_file) from 
@@ -210,7 +169,7 @@ def insertionForDate(date_to_work_with):
             sql_f.write('''copy updates_y{} (update_date, update_time, upd_type, 
                             bgp_neighbor, peeras, prefix, source_file) from 
                             '{}' WITH (FORMAT csv);\n'''\
-                            .format(date_to_work_with.year, updates_csv))
+                            .format(day_before.year, updates_csv))
     
     sys.stdout.write('{}: SQL file generated. Inserting into the DB.\n'.format(datetime.now()))
     
@@ -234,8 +193,6 @@ def insertionForDate(date_to_work_with):
     sys.stdout.write('{}: Removing SQL file {}.\n'.format(datetime.now(), sql_file))
     os.remove(sql_file)
     
-    return readable_complete
-
 def main(argv):
     date_to_work_with = ''
 
